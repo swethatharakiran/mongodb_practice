bea2cf61e74996f303f957b18043d00365a50914
swethatharakiran <115216446+swethatharakiran@users.noreply.github.com>
2023-02-07 13:38:52 +0530

    place orders

--- models/user.js
+++ models/user.js
@@ -64,7 +64,36 @@ class User {
            {$set:{cart:{items:updatedCartItems}}})
    
    }
-   
+
+   addOrder(){
+    const db=getDb();
+    return this.getCart().then(products=>{
+      const order={
+        items:products,
+        user:{
+          _id:new mongodb.ObjectId(this._id),
+          username:this.username,
+          email:this.email
+        }
+      }
+      return db.collection('orders').insertOne(order)
+
+    })
+    .then(result=>{
+      this.cart={items:[]};
+      return db.collection('users').updateOne(
+        {_id:new mongodb.ObjectId(this._id)},
+        {$set:{cart:{items:[]}}})
+    });
+   }
+
+   getOrders(){
+
+    const db=getDb();
+    return db.collection('orders')
+    .find({'user._id':new mongodb.ObjectId(this._id)})
+    .toArray()
+   }
     static findById(userid){
       const db=getDb();
       return db.collection('users').findOne({_id:new mongodb.ObjectId(userid)})
